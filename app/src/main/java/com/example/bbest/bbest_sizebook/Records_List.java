b343bfbd29a61f7c685aafa1ac25cb9aac287bec
BBest <bbest@ualberta.ca>
2017-01-25 17:49:10 -0700

    Constructed Records list start page.

--- app/src/main/java/com/example/bbest/bbest_sizebook/Records_List.java
+++ app/src/main/java/com/example/bbest/bbest_sizebook/Records_List.java
@@ -2,12 +2,30 @@ package com.example.bbest.bbest_sizebook;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class Records_List extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Button AddPersonButton;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_records__list);
+        AddPersonButton = (Button) findViewById(R.id.AddRecordButton);
+        AddPersonButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Toast.makeText(getBaseContext(),"Clicked on Add button", Toast.LENGTH_LONG).show();
+            }
+        });
     }
+
+    //onClick method for AddButton to go to Add Person Activity
+    public void addPerson(View v){
+
+
+    }
+
 }
