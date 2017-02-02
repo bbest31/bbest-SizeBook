c99a2d98fdbb9fa6ca8cc30de6ef64873027272c
BBest <bbest@ualberta.ca>
2017-02-02 00:31:31 -0700

    Added getPerson() method to RecordsList model to finish model coding

--- app/src/main/java/com/example/bbest/bbest_sizebook/RecordsList.java
+++ app/src/main/java/com/example/bbest/bbest_sizebook/RecordsList.java
@@ -3,7 +3,7 @@ import java.util.Collection;
 import java.util.Observable;
 import java.util.ArrayList;
 /**
- * Records List class to hold all the current records of com.example.bbest.bbest_sizebook.Person objects
+ * Records List class to hold all the current records of Person objects
  * Created by bbest on 01/02/17.
  */
 
@@ -17,19 +17,28 @@ public class RecordsList extends Observable {
 
     }
 
-    public Collection<Person> getPersons(){
+// gets all of the Person records
+    public Collection<Person> getRecords(){
 
         return recordList;
     }
 
+//adds a new peron to the list
     public void addRecord(Person newPerson){
 
         recordList.add(newPerson);
     }
 
+//removes a Person from the list
     public void removePerson(Person p){
         recordList.remove(p);
-        
+
     }
 
+//returns a Person object from the list, used for editing a specific Person object
+    public Person getPerson(Person person){
+
+        int personIndex = recordList.indexOf(person);
+        return recordList.get(personIndex);
+    }
 }
